5ad24bfdab6fb3f1928c385f6752020a941d6dfe
Moji Raznahan <moji.rzn@gmail.com>
2025-01-31 13:32:45 -0800

    initial commit

---
+++ src/main/java/com/example/CDSpringBoot/CdSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.CDSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CdSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/CDSpringBoot/CdSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CDSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
